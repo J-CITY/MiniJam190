93d54cc55a6a3dfc2d2f0ac9eb1003668a3a69ea
JIukaviy <jiukaviy@gmail.com>
2025-08-03 12:43:19 +0300

    Register QTE MiniGame

---
+++ Assets/Scripts/MiniGames/QTE/QTESpawner.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class QTESpawner : MonoBehaviour
+{
+    public GameObject qtePrefab;
+
+    void Start()
+    {
+        var UI = GameObject.Find("UI");
+        Instantiate(qtePrefab, UI.transform);
+    }
+}
